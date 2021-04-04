5e00eb4f3fcf0ebd0750490fe505ee76481b1bea
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-04-04 10:30:59 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//first I will need to implement and link all of my prerequisite files  -----------
+//(javascript, jQuery, moment, OpenWeatherAPI, CSS, Bootstrap) ------
+//Then I will build my basic HTML elements so that they can be styled to 
+//make room for the appended elements
+//Then I will make a form (prevent default) that has a search bar and button
+//When the button is pressed, if the text field has content that matches data from openWeatherAPi, it will show that city and its weather forecast as 
+//it will save the name of that city and the weather conditions in the local storage as a variable
+//after the button is pressed it will make a button with the name of the city that will make a request for the forecast upon click of that button
