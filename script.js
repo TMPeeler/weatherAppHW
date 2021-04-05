3bf3a4edd090d5b7a693e35c17046cbe601cb4d5
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-04-04 20:21:14 -0700

    continue pseudocoding

--- script.js
+++ script.js
@@ -6,3 +6,17 @@
 //When the button is pressed, if the text field has content that matches data from openWeatherAPi, it will show that city and its weather forecast as 
 //it will save the name of that city and the weather conditions in the local storage as a variable
 //after the button is pressed it will make a button with the name of the city that will make a request for the forecast upon click of that button
+
+
+// set variables and grab dom elements for the current weather, the 5 day forecast, and the search history
+
+// fetch weather, 5 day forecast, and uv index from API
+
+// render the weather, the forecast, and the uv index (color green, yellow, red)
+
+// set any text entered in the text field to store as a variable
+
+// set search history items to be parsed and appended to the page from local storage
+
+
+
